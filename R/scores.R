f8420d5228659c4cad56afbd5d1ca10b20921fb5
Brooke <brookiemb@gmail.com>
2018-01-29 16:27:07 -0700

    starting to change the main functions

--- R/scores.R
+++ R/scores.R
@@ -10,6 +10,7 @@
 #' @param minus1 do we want to minus 1 from the raw scores; \code{default = TRUE}
 #'
 #' @import dplyr
+#' @import furniture
 #'
 #' @export
 score.docs <- function(data, germs, harm, thoughts, symmetry, minus1 = TRUE){
@@ -23,10 +24,31 @@ score.docs <- function(data, germs, harm, thoughts, symmetry, minus1 = TRUE){
   }
 
   data %>%
-    mutate(docs.germs = rowSums(data[, germs])) %>%
+    mutate(docs.germs = furniture::rowsums()) %>%
     mutate(docs.harm = rowSums(data[, harm])) %>%
     mutate(docs.thoughts = rowSums(data[, thoughts])) %>%
     mutate(docs.symmetry = rowSums(data[, symmetry])) %>%
     mutate(docs = docs.germs + docs.harm + docs.thoughts + docs.symmetry)
 
 }
+
+
+#' Score Docs
+#'
+#' It scores the DOCS (the Dimensional Obsessive-Compulsive Scale).
+#'
+#' @param data the data frame
+#' @param germs the germs subscale
+#' @param harm the harm subscale
+#' @param thoughts the thoughts subscale
+#' @param symmetry the symmetry subscale
+#' @param minus1 do we want to minus 1 from the raw scores; \code{default = TRUE}
+#'
+#' @import dplyr
+#'
+#' @export
+
+
+
+
+
